d6dfbd6bbe06d23a682ceaf0a49c09518770292d
Artsiom Korenko <artemich1827284@gmail.com>
2022-08-09 10:12:30 +0300

    Add project

---
+++ Used.by/Used.by/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Used.by
+//
+//  Created by Artsiom Korenko on 9.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
